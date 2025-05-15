adc3bbecc7c2d33f2bb9033cc7243dfb84b68da6
Ravi Ranjan Kumar <ravikumar.inbh@gmail.com>
2025-05-16 00:26:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // Future use: animations, form validation, etc.
 console.log("Portfolio loaded");
+
+
+function doPost(e) {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var data = JSON.parse(e.postData.contents);
+
+  sheet.appendRow([
+    new Date(),
+    data.firstName,
+    data.lastName,
+    data.organization,
+    data.email,
+    data.contact,
+    data.message
+  ]);
+
+  return ContentService.createTextOutput(
+    JSON.stringify({ result: "Success", data: data })
+  ).setMimeType(ContentService.MimeType.JSON);
+}
